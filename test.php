fd7392bb5cc2ebf707adec36b144514e75580751
ealuna <edwlunan@gmail.com>
2017-03-14 15:43:50 -0500

    Guardar

--- test.php
+++ test.php
@@ -1,10 +1,23 @@
 <?php
-$as = trim($_SERVER['PATH_INFO'],'/');
+require_once 'connection.php';
+$as = trim($_SERVER['PATH_INFO'], "/");
 $request = explode('/', $as);
-//array_shift($request);
+
 //echo $as;
-var_dump($request);
-echo $_SERVER['PATH_INFO']."\n";
-echo $_SERVER['REQUEST_URI'];
+print_r($request);
+$req1 = preg_replace('/[^a-z0-9_]+/i','',$request);
+print_r($req1);
+//echo $_SERVER['PATH_INFO']."\n";
+$link = connection::get_connection()->get_data_base();
+
+  $values = array_map(function ($value) use ($link) {
+    if ($value===null) {
+      return null;
+    }else{
+      return $link->quote((string)$value);
+    }  
+  },array_values($request));
+
+  print_r($values);
 
 ?>
